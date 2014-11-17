1cbf3bd02236187a27a9e86dd1d1f21e841d8866
Krystian Paszek <krystianpaszek93@gmail.com>
2014-11-17 11:41:54 +0100

    Porzadki w kodzie, wywalilem polskie znaki

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 print "Importing libraries..."
 import sys
-#to działa typowo u mnie, nie powinno to tak wyglądać :P
 sys.path.append('/usr/local/lib/python2.7/site-packages')
 import zbar
 import cv2
-from PIL import Image
-#ImageOps w sumie nie jest potrzebne, używałem tego do debugu
-import PIL.ImageOps
+from PIL import Image, ImageDraw
 import time
 import thread
 
@@ -15,8 +12,6 @@ cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
 if vc.isOpened():
-    #pierwszy odczyt, nie wiem w sumie po co jest osobno od całej pętli
-    #można spróbować się tego pozbyć
     rval, frame = vc.read()
 else:
     rval = False
@@ -24,14 +19,12 @@ else:
 print "Creating scanner object..."
 scanner = zbar.ImageScanner()
 scanner.parse_config('enable')
-#konwersja z przedziwnego BGR którym posługuje się OpenCV
-rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#konwersja na PIL Image którym posługuje się zbar
-#.convert('L') chyba przekonwertowuje na odcienie szarości, ważne by zbar dobrze odczytywał
-i = Image.fromarray(rgb, 'RGB').convert('L')
 
 def process_image():
+    #konwersja z przedziwnego BGR ktorym posluguje sie OpenCV
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    #konwersja na PIL Image ktorym posluguje sie zbar
+    #.convert('L') przekonwertowuje na odcienie szarosci, wazne by zbar dobrze odczytywal
     i = Image.fromarray(rgb, 'RGB').convert('L')
     width, height = i.size
     raw = i.tostring()
@@ -39,7 +32,7 @@ def process_image():
     image = zbar.Image(width, height, 'Y800', raw)
     #skanowanie obrazu
     scanner.scan(image)
-    #i wypisanie zakodowanych treści
+    #i wypisanie zakodowanych tresci
     for symbol in image:
         print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
     del(image)
@@ -48,14 +41,13 @@ def process_image():
 print "Starting capture..."
 while rval:
     #multithreading taki trudny
-    #dzięki temu podgląd mamy na żywo, a przetwarzanie jest w tle
+    #dzieki temu podglad mamy na zywo, a przetwarzanie jest w tle
+    rval, frame = vc.read()
     thread.start_new_thread(process_image, ())
     cv2.imshow("preview", frame)
-    rval, frame = vc.read()
     key = cv2.waitKey(20)
     if key == 27:
         print "Stopping capture"
         break
-    #time.sleep(1)
 
 cv2.destroyWindow("preview")
